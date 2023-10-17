8429160f468f457404e6944b1d425fc96812cca3
kuku <kuku@kuku.me>
2023-10-17 20:46:18 +0800

    to maven

--- ruoyi-generator/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-dependencies {
-    api("org.apache.velocity:velocity-engine-core")
-    api("commons-collections:commons-collections")
-    api(project(":ruoyi-common"))
-}
--- ruoyi-quartz/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-dependencies {
-    api("org.quartz-scheduler:quartz") {
-        exclude(group = "com.mchange", module = "c3p0")
-    }
-    api(project(":ruoyi-common"))
-}
